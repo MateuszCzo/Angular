6e58ffe6ab8cede2b72f3fcb5fd16b52438119cf
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-04-23 17:54:04 +0200

    Wyswietlanie danych - ngFor

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -6,8 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  days = ['poniedzialek', 'wtorek', 'sroda', 'czwartek', 'piatek', 'sobota', 'niedziela'];
 
-  dog: Dog;
+  dogs = new Array<Dog>();
+
+  constructor() {
+    this.dogs.push(
+      new Dog('asd', 4),
+      new Dog('sdf', 5),
+      new Dog('dfg', 6)
+    )
+  }
 }
 
 class Dog {
