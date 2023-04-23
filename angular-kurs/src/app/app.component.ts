dcec6d7830e7c67a31d0f40e7cfbd2b266d8faa3
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-04-23 17:18:41 +0200

    Wyswietlanie danych

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-kurs';
+  title = "Angular";
+
+  dog = new Dog("Reksio", 4);
+
+  showDog() {
+    return ' Mój pies to ' + this.dog.name + ' ma ' + this.dog.age + ' lata.'
+  }
+}
+
+class Dog {
+  constructor(public name: string, public age: number) {
+  }
 }
--- angular-kurs/src/app/test2/test2.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-test2',
-  template: '<app-test></app-test>'
-})
-export class Test2Component {
-
-}
