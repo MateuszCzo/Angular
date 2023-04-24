8329e5e506c0e5de16d59e0a414903ba93642339
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-04-24 20:07:07 +0200

    Wyswietlanie danych - ngSwitch

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -8,33 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   days = ['poniedzialek', 'wtorek', 'sroda', 'czwartek', 'piatek', 'sobota', 'niedziela'];
 
-  dogs = new Array<Dog>();
-
-  constructor() {
-    this.dogs.push(
-      new Dog('asd', 4),
-      new Dog('sdf', 5),
-      new Dog('dfg', 6)
-    )
-  }
-
-  addDogs() {
-    if(this.dogs.length > 0) return;
-    this.dogs.push(
-      new Dog('asd', 4),
-      new Dog('sdf', 5),
-      new Dog('dfg', 6)
-    )
-  }
-
-  removeDogs() {
-    if(this.dogs.length == 0) return;
-    this.dogs.splice(0, this.dogs.length);
-  }
-
-}
-
-class Dog {
-  constructor(public name: string, public age: number) {
-  }
 }
