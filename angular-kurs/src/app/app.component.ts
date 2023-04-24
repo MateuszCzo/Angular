2bf39d833dab6c0756a14e8daaad53cecba5874a
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-04-24 19:59:19 +0200

    Wyswietlanie danych - ngIf, then, else, ng-template

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -17,6 +17,21 @@ export class AppComponent {
       new Dog('dfg', 6)
     )
   }
+
+  addDogs() {
+    if(this.dogs.length > 0) return;
+    this.dogs.push(
+      new Dog('asd', 4),
+      new Dog('sdf', 5),
+      new Dog('dfg', 6)
+    )
+  }
+
+  removeDogs() {
+    if(this.dogs.length == 0) return;
+    this.dogs.splice(0, this.dogs.length);
+  }
+
 }
 
 class Dog {
