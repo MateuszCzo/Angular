01a205a5a82ccf709794b18cfcc063119c010ae0
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-04-25 22:14:47 +0200

    Interakcja komponentow - @Output()

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     tasksList: Array<string> = ['task1', 'task2', 'task3'];
+
+    selected(task: string): void {
+      console.log(task);
+    }
 }
--- angular-kurs/src/app/child/child.component.ts
+++ angular-kurs/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,7 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   @Input('tasksProp') tasks: Array<string> = [];
+  @Output() eventTask = new EventEmitter<string>();
 
   ngOnInit() {
+  }
+
+  select(task : string) {
+    this.eventTask.emit(task);
   }
 }
