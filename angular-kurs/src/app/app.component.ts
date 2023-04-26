88a59df25f7d27b8b5196e525bf2971f1ba2f3b1
Mateusz Czosnyka <mateuszczosn@gmail.com>
2023-04-26 22:38:29 +0200

    Cykle zycia komponentu

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  title = 'Lorem ipsum...';
+  dog = new Dog('Reksio');
+  show = true;
+
+  ngOnInit(): void {
+  }
+
+  changeTitle() {
+    this.title = 'Lorem ipsum 2...';
+  }
+
+  changeDog() {
+    //this.dog.name = 'Pluto'; // nie zadziala ngOnChanges w komponencie child
+    this.dog = new Dog('Pluto');
+  }
+
+  nothing() {
+  }
+
+  destroy() {
+    this.show = !this.show;
+  }
+}
+
+class Dog {
+  constructor(public name: string) {
+  }
 }
--- angular-kurs/src/app/child/child.component.ts
+++ angular-kurs/src/app/child/child.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import {  AfterContentChecked, 
+          AfterContentInit, 
+          AfterViewChecked, 
+          AfterViewInit, 
+          Component, 
+          DoCheck, 
+          Input, 
+          OnChanges, 
+          OnInit, 
+          SimpleChanges,
+          OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent implements  OnChanges, 
+                                        OnInit, 
+                                        DoCheck,
+                                        AfterContentInit, 
+                                        AfterContentChecked, 
+                                        AfterViewInit, 
+                                        AfterViewChecked,
+                                        OnDestroy {
+  @Input() inputText: string = '';
+  @Input() inputDog: Dog = new Dog('');
+  show = true;
 
+  /**
+   * Uruchamia sie na poczatku, przed ngOnInit
+   * Sprawdza czy zmienily sie zbindowane pola komponentu
+   * Musi zmienic sie referencja
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges - uruchomienie - #1');
+    console.log(changes);
+  }
+
+  /**
+   * Uruchamia sie jeden raz podczas inicjalizacji komponentu
+   * Uruchamia sie po konstruktorze i po ngOnChange
+   */
+  ngOnInit(): void {
+    console.log('ngOnInit - uruchomienie - #2');
+  }
+
+  /**
+   * Uruchamia sie przy kazdej zmianie, wywolaniu eventu
+   */
+  ngDoCheck(): void {
+    console.log('ngDoCheck - uruchomienie - #3');
+  }
+
+  /**
+   * Uruchamia sie po inicjalizacji np <ng-content>
+   */
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit - uruchomienie - #4');
+  }
+
+  /**
+   * Uruchamia sie po kazdej zmianie np <ng-content>
+   */
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked - uruchomienie - #5');
+  }
+
+  /**
+   * Uruchamia sie po inicjalizacji widoku
+   */
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit - uruchomienie - #6');
+  }
+
+  /**
+   * Uruchamia sie po kazdej zmianie widoku
+   */
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked - uruchomienie - #7');
+  }
+
+  /**
+   * Uruchamia sie po zniszczeniu widoku
+   */
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy - uruchomienie - #8');
+  }
+
+  content() {
+    this.show = !this.show;
+  }
+}
+
+class Dog {
+  constructor(public name: string) {
+  }
 }
